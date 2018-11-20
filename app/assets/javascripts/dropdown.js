4bf1711643ae92d3acf630ac4069fb229cf475cc
Fays <fay.meghzili@gmail.com>
2018-11-20 15:05:30 +0100

    dropdown

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var state= document.getElementById("myDropdown");
+    if (state.style.display=="none") {
+        state.style.display="block"
+    }
+    else {
+        state.style.display="none"
+    }
+
+}
